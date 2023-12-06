bf1ae8941cbb01dcb92e9aaedfbd3b307859f5fd
Eliot <a7868783@gmail.com>
2023-12-06 13:46:45 +0800

    clerk installation environment test ( ok )

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,10 @@
+async function Page(){
+    return (
+        <main>
+            <h1 className="head-text">Onboarding</h1>
+            <p>Onboarding page</p>
+        </main>
+    )
+}
+
+export default Page;
